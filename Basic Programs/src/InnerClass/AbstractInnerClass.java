3b32dc3be4c69ff5fd893950dc27502d01690286
mustafamohammad01 <109622842+mustafamohammad01@users.noreply.github.com>
2022-07-27 19:47:35 +0530

    Assisted practice programs

--- Basic Programs/src/InnerClass/AbstractInnerClass.java
+++ Basic Programs/src/InnerClass/AbstractInnerClass.java
@@ -1,5 +1,22 @@
 package InnerClass;
 
-public class AbstractInnerClass {
+public abstract class AbstractInnerClass {
+	 public abstract void display();
+	   void show(){
+		System.out.println("show method");   
+	   }
+	}
 
+
+class innerClassAssisted3 {
+
+	public static void main(String[] args) {
+		AbstractInnerClass i = new AbstractInnerClass() {
+
+	         public void display() {
+	            System.out.println("Anonymous Inner Class");
+	         }
+	      };
+	      i.display();
+	   }
 }
